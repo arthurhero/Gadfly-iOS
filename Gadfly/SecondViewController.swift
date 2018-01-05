3d10537ad797989e4f55fb340fd27feaa3590e0e
arthurhero <chenziwe@grinnell.edu>
2018-01-05 13:12:03 -0600

    Initial Commit

---
+++ Gadfly/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Gadfly
+//
+//  Created by Ziwen Chen on 1/5/18.
+//  Copyright © 2018 G-FORVM. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
