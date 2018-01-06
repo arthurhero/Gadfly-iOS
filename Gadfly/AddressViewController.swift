9250f92feb80153bfe91162e9267bccb7ebc02b8
arthurhero <chenziwe@grinnell.edu>
2018-01-05 20:35:29 -0600

    finished map

--- Gadfly/AddressViewController.swift
+++ Gadfly/AddressViewController.swift
@@ -7,15 +7,74 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
-class AddressViewController: UIViewController {
+class AddressViewController: UIViewController, UISearchBarDelegate, LocateOnTheMap {
 
+    @IBOutlet weak var mapContainer: UIView!
+    
+    var googleMapsView : GMSMapView!
+    var searchResultController : SearchResultsController!
+    var resultsArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        self.googleMapsView = GMSMapView(frame: self.mapContainer.frame)
+        self.view.addSubview(self.googleMapsView)
+        
+        self.searchResultController = SearchResultsController()
+        searchResultController.delegate = self
+    }
 
+    @IBAction func searchWithAddress(_ sender: Any) {
+        let searchController = UISearchController(searchResultsController: searchResultController)
+        searchController.searchBar.delegate = self
+        self.present(searchController, animated: true, completion: nil)
+    }
+    
+    func locateWithLongitude(_ lon: Double, andLatitude lat: Double, andTitle title: String) {
+        DispatchQueue.main.sync {
+            let position = CLLocationCoordinate2DMake(lat, lon)
+            let marker = GMSMarker(position: position)
+            
+            let camera = GMSCameraPosition.camera(withLatitude: lat, longitude: lon, zoom: 10)
+            self.googleMapsView.camera = camera
+            
+            marker.title = nil
+            marker.map = self.googleMapsView
+        }
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let filter = GMSAutocompleteFilter()
+        filter.type = .establishment
+        filter.country = "US"
+
+        let placeClient = GMSPlacesClient()
+        placeClient.autocompleteQuery(searchText, bounds: nil, filter: filter) { (results, error) in
+            self.resultsArray.removeAll()
+            if results == nil {
+                return
+            }
+
+            for result in results! {
+                if let result = result as? GMSAutocompletePrediction {
+                    self.resultsArray.append(result.attributedFullText.string)
+                }
+            }
+            
+            self.searchResultController.reloadDataWithArray(self.resultsArray)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
