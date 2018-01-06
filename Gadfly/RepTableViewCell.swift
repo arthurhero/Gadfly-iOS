f685961b5aa3b2b9bb2a7bc71f98f0a2e236f94d
arthurhero <chenziwe@grinnell.edu>
2018-01-05 19:49:15 -0600

    added files

---
+++ Gadfly/RepTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RepTableViewCell.swift
+//  Gadfly
+//
+//  Created by Ziwen Chen on 1/5/18.
+//  Copyright © 2018 G-FORVM. All rights reserved.
+//
+
+import UIKit
+
+class RepTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
