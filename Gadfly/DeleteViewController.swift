d5ca4bc3b5957027b4f2074db6a7f40c049d39fc
arthurhero <chenziwe@grinnell.edu>
2018-01-11 19:44:02 -0600

    finished delete script page

--- Gadfly/DeleteViewController.swift
+++ Gadfly/DeleteViewController.swift
@@ -9,13 +9,67 @@
 import UIKit
 
 class DeleteViewController: UIViewController {
+    
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    func enterLoadingMode() {
+        submitButton.isEnabled = false
+        cancelButton.isEnabled = false
+    }
+    
+    func enterNormalMode() {
+        submitButton.isEnabled = true
+        cancelButton.isEnabled = true
+    }
+    
+    @IBOutlet weak var ticketTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        enterNormalMode()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        enterLoadingMode()
+        
+        let ticket : String = ticketTextField.text!
+        print(ticket)
+        
+        GFScript.delete(withTicket: ticket) { (result) in
+            let status : String = result?["Status"] as! String
+            if (status != "OK") {
+                DispatchQueue.main.sync {
+                    let alert : UIAlertController = UIAlertController(title: "FAIL",
+                                                                      message: "Fail to delete this script\n\nServer: " + status, preferredStyle: UIAlertControllerStyle.alert)
+                    let defaultAction : UIAlertAction! = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { (action) in
+                        self.enterNormalMode()
+                    })
+                    alert.addAction(defaultAction)
+                    self.present(alert, animated: true, completion: nil)
+                }
+                
+            } else {
+                DispatchQueue.main.sync {
+                    let alert : UIAlertController = UIAlertController(title: "SUCCESS",
+                                                                      message: "Deleted Successfully!", preferredStyle: UIAlertControllerStyle.alert)
+                    let defaultAction : UIAlertAction! = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { (action) in
+                        self.enterNormalMode()
+                    })
+                    alert.addAction(defaultAction)
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
