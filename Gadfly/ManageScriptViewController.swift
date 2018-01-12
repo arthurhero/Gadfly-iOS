f4000b05449062d5b0f5c406c0459dca011ecd8e
arthurhero <chenziwe@grinnell.edu>
2018-01-11 18:24:01 -0600

    added delete script view

---
+++ Gadfly/ManageScriptViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ManageScriptViewController.swift
+//  Gadfly
+//
+//  Created by Ziwen Chen on 1/11/18.
+//  Copyright © 2018 G-FORVM. All rights reserved.
+//
+
+import UIKit
+
+class ManageScriptViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
